03001b21422f7be588c22148df3374028477b66a
Kendal Asprec <kenasprec@berkeley.edu>
2018-01-21 06:01:39 -0800

    Added some animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-    
+
+    $(function () {
+        if (window.location.pathname == "/Fashare/index.html") {
+            $('#navbar-search-tile').hide();
+        } else {
+            $('#navbar-search-tile').show();
+        }
+    });
+
+    var topOfOthDiv = $("#breakpoint").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#navbar-search-tile").fadeIn(200); //reached the desired point -- show div
+        } else {
+            $("#navbar-search-tile").fadeOut(200);
+        }
+    });
+
 });
