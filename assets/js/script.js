002f9374ce71629c7e921db5177d82c8a74d4157
Kendal Asprec <kenasprec@berkeley.edu>
2018-01-21 06:12:33 -0800

    Fixed JavaScript error

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,16 @@ $(document).ready(function () {
     $(function () {
         if (window.location.pathname == "/Fashare/index.html") {
             $('#navbar-search-tile').hide();
+            var topOfOthDiv = $("#breakpoint").offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+                    $("#navbar-search-tile").fadeIn(200); //reached the desired point -- show div
+                } else {
+                    $("#navbar-search-tile").fadeOut(200);
+                }
+            });
         } else {
             $('#navbar-search-tile').show();
-        }
-    });
-
-    var topOfOthDiv = $("#breakpoint").offset().top;
-    $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-            $("#navbar-search-tile").fadeIn(200); //reached the desired point -- show div
-        } else {
-            $("#navbar-search-tile").fadeOut(200);
         }
     });
 
